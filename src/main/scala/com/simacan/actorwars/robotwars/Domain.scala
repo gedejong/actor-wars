98ce56147a7b1f5720b6523d501903acbe009c7e
Edwin de Jong <edwin.de.jong@topicus.nl>
2017-11-23 19:25:11 +0100

    Game settings

--- src/main/scala/com/simacan/actorwars/robotwars/Domain.scala
+++ src/main/scala/com/simacan/actorwars/robotwars/Domain.scala
@@ -9,14 +9,14 @@ object Domain {
   sealed trait GameObject
 
   case class Robot(
-                  color: Color,
-                  x: Double = 0, y: Double = 0, dx: Double = 0, dy: Double = 0,
-                  enginePower: Double = 0,
-                  heading: Double = 0,
-                  hitpoints: Int = 10,
-                  steerPos: Double = 0,
-                  coolDown: Double = 0,
-                  firing: Boolean = false) extends GameObject
+                    color: Color,
+                    x: Double = 0, y: Double = 0, dx: Double = 0, dy: Double = 0,
+                    enginePower: Double = 0,
+                    heading: Double = 0,
+                    hitpoints: Int = 10,
+                    steerPos: Double = 0,
+                    coolDown: Double = 0,
+                    firing: Boolean = false) extends GameObject
 
   case class Bullet(x: Double = 0, y: Double = 0, dx: Double = 0, dy: Double = 0, color: Color) extends GameObject
 
@@ -38,10 +38,32 @@ object Domain {
   case class GameOver(winners: Seq[Player]) extends GameState
 
 
+  sealed trait ClientMessage
+
+  case class Register(listener: ActorRef, name: String) extends ClientMessage
+
+  case class RobotCommand(enginePower: Double, steerPosition: Double, fire: Boolean) extends ClientMessage
+
+  case class RobotSettings(
+                            width: Double,
+                            height: Double,
+                            initialHitpoints: Int,
+                            coolDownMillis: Double,
+                            maximumSteerPos: Double,
+                            maximumEnginePower: Double,
+                            bulletSpeed: Double)
+
+  case class LevelSettings(
+                            width: Double,
+                            height: Double,
+                            robotSettings: RobotSettings,
+                            maximumRobots: Int)
+
   sealed trait ServerMessage
+
   case class GameTick(gameState: GameState) extends ServerMessage
 
-  sealed trait ClientMessage
-  case class Register(listener: ActorRef, name: String) extends ClientMessage
-  case class RobotCommand(enginePower: Double, steerPosition: Double, fire: Boolean) extends ClientMessage
+  case class GameSettings(levelSettings: LevelSettings) extends ServerMessage
+
 }
+
